733f31821f4ac7fa73bbcd11bf2b2a0c228dabfd
ee\viktorm <none@none>
2013-06-13 17:20:03 +0300

    missing_fundamental

---
+++ missing_fundamental.py
@@ -0,0 +1,18 @@
+
+import utils
+from rationals import Rational 
+
+def getFundamentals(harmonics):
+    """
+    spectrum to spectrum
+    [(int, float)] -> [(int, float)]
+    """
+    pass
+
+
+def test():
+    pass
+
+if __name__ == "__main__":
+    test()
+    
